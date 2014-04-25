6eda4fc73c6874e2e0ed49952dd522fe76259606
burton <burt.alexander@gmail.com>
2014-04-25 14:59:00 +1000

    Added remote testing

--- ESP-ThinkGear/src/com/github/mrstampy/esp/neurosky/ThinkGearTester.java
+++ ESP-ThinkGear/src/com/github/mrstampy/esp/neurosky/ThinkGearTester.java
@@ -1,10 +1,24 @@
 package com.github.mrstampy.esp.neurosky;
 
+import java.io.IOException;
+
+import com.github.mrstampy.esp.mutliconnectionsocket.MultiConnectionSocketException;
+import com.github.mrstampy.esp.neurosky.event.EventType;
 import com.github.mrstampy.esp.neurosky.subscription.ThinkGearEventListenerAdapter;
+import com.github.mrstampy.esp.neurosky.subscription.ThinkGearSocketConnector;
 
 public class ThinkGearTester {
 
 	public static void main(String[] args) throws Exception {
+		if(args.length == 0) {
+			testLocalAggregation();
+		} else {
+			testRemoteAggregation();
+		}
+	}
+
+	private static void testLocalAggregation() throws IOException, MultiConnectionSocketException, InterruptedException {
+		System.out.println("Local Aggregation");
 		MultiConnectionThinkGearSocket thinkGearSocket = new MultiConnectionThinkGearSocket("localhost");
 		thinkGearSocket.setRawData(true);
 		ThinkGearEventListenerAdapter adapter = new ThinkGearEventListenerAdapter();
@@ -12,6 +26,27 @@ public class ThinkGearTester {
 
 		thinkGearSocket.start();
 
+		printSampleLengths(adapter);
+	}
+
+	private static void testRemoteAggregation() throws Exception {
+		System.out.println("Remote Aggregation");
+		MultiConnectionThinkGearSocket thinkGearSocket = new MultiConnectionThinkGearSocket("localhost", true);
+		thinkGearSocket.setRawData(true);
+
+		ThinkGearSocketConnector connector = new ThinkGearSocketConnector("localhost");
+		ThinkGearEventListenerAdapter adapter = new ThinkGearEventListenerAdapter();
+		connector.addThinkGearEventListener(adapter);
+
+		connector.connect();
+		connector.subscribe(EventType.rawEeg);
+
+		thinkGearSocket.start();
+
+		printSampleLengths(adapter);
+	}
+
+	private static void printSampleLengths(ThinkGearEventListenerAdapter adapter) throws InterruptedException {
 		while (true) {
 			Thread.sleep(1000);
 			double[][] sampled = adapter.getCurrentSecondOfSampledData();
